ae6cf1054680cee5b5eb958494c2163aa5aa44db
Pavlo Melnyk <pavel.melnyk10@gmail.com>
2024-11-17 02:39:44 +0000

    Initial commit with project setup, Checkstyle, and CI configuration

---
+++ src/main/java/mate/academy/bookstore/SpringBookstoreApplication.java
@@ -0,0 +1,12 @@
+package mate.academy.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBookstoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBookstoreApplication.class, args);
+    }
+
+}
