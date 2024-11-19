37f997d23c2c85a8860c991e2fc88c466f645d31
Pavlo Melnyk <pavel.melnyk10@gmail.com>
2024-11-19 14:29:54 +0000

    Revert "refactor: replaced @Autowired with @RequiredArgsConstructor for cleaner dependency injection"
    This reverts commit 05c9206e5653916fcb80088b312ab413bd5febd0.

--- src/main/java/mate/academy/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/bookstore/service/impl/BookServiceImpl.java
@@ -1,39 +1,24 @@
 package mate.academy.bookstore.service.impl;
 
 import java.util.List;
-import lombok.RequiredArgsConstructor;
-import mate.academy.bookstore.dto.BookDto;
-import mate.academy.bookstore.dto.CreateBookRequestDto;
-import mate.academy.bookstore.mapper.BookMapper;
 import mate.academy.bookstore.model.Book;
 import mate.academy.bookstore.repository.BookRepository;
 import mate.academy.bookstore.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
     @Autowired
     private BookRepository bookRepository;
-    private final BookMapper bookMapper;
 
     @Override
-    public BookDto save(CreateBookRequestDto requestDto) {
-        Book book = bookMapper.toModel(requestDto);
-        return bookMapper.toDto(bookRepository.save(book));
+    public Book save(Book book) {
+        return bookRepository.save(book);
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll().stream()
-                .map(bookMapper::toDto)
-                .toList();
-    }
-
-    @Override
-    public BookDto getById(Long id) {
-        Book book = bookRepository.findById(id);
-        return bookMapper.toDto(book);
+    public List<Book> findAll() {
+        return bookRepository.findAll();
     }
 }
