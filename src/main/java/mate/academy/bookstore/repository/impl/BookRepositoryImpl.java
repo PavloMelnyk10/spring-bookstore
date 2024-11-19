2839f0fc66d2da242d3926299d72870076d39661
Pavlo Melnyk <pavel.melnyk10@gmail.com>
2024-11-19 15:44:38 +0000

    feat: added BookDto as response DTO and CreateBookRequestDto as request DTO. Integrated MapStruct. Updated service layer to return and accept DTOs. Implemented BookController with methods: getAll(), getBookById(Long id), and createBook(CreateBookRequestDto). Added missing methods in BookService and BookRepository. Created and used EntityNotFoundException.

--- src/main/java/mate/academy/bookstore/repository/impl/BookRepositoryImpl.java
+++ src/main/java/mate/academy/bookstore/repository/impl/BookRepositoryImpl.java
@@ -1,49 +1,48 @@
 package mate.academy.bookstore.repository.impl;
 
+import jakarta.persistence.EntityManager;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.bookstore.exception.DataProcessingException;
+import mate.academy.bookstore.exception.EntityNotFoundException;
 import mate.academy.bookstore.model.Book;
 import mate.academy.bookstore.repository.BookRepository;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @RequiredArgsConstructor
 public class BookRepositoryImpl implements BookRepository {
-    private final SessionFactory sessionFactory;
+    private final EntityManager entityManager;
 
     @Override
+    @Transactional
     public Book save(Book book) {
-        Session session = null;
-        Transaction transaction = null;
         try {
-            session = sessionFactory.openSession();
-            transaction = session.beginTransaction();
-            session.persist(book);
-            transaction.commit();
+            entityManager.persist(book);
             return book;
         } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
             throw new DataProcessingException("Failed to add book: " + book, e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
         }
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Book> findAll() {
-        try (Session session = sessionFactory.openSession()) {
-            return session.createQuery("FROM Book", Book.class).getResultList();
+        try {
+            return entityManager.createQuery("FROM Book", Book.class).getResultList();
         } catch (Exception e) {
             throw new DataProcessingException("Can't get all books", e);
         }
     }
 
+    @Override
+    @Transactional(readOnly = true)
+    public Book findById(Long id) {
+        Book book = entityManager.find(Book.class, id);
+        if (book == null) {
+            throw new EntityNotFoundException("Can't find book with id: " + id);
+        }
+        return book;
+    }
 }
--- src/main/java/mate/academy/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/mate/academy/bookstore/service/impl/BookServiceImpl.java
@@ -2,25 +2,36 @@ package mate.academy.bookstore.service.impl;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import mate.academy.bookstore.dto.BookDto;
+import mate.academy.bookstore.dto.CreateBookRequestDto;
+import mate.academy.bookstore.mapper.BookMapper;
 import mate.academy.bookstore.model.Book;
 import mate.academy.bookstore.repository.BookRepository;
 import mate.academy.bookstore.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class BookServiceImpl implements BookService {
-    @Autowired
     private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public BookDto save(CreateBookRequestDto requestDto) {
+        Book book = bookMapper.toModel(requestDto);
+        return bookMapper.toDto(bookRepository.save(book));
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public BookDto getById(Long id) {
+        Book book = bookRepository.findById(id);
+        return bookMapper.toDto(book);
     }
 }
