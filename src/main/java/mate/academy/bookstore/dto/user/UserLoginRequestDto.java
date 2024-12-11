d9254f737d88fb5f4edd50eb97b457c11235f967
Pavlo Melnyk <pavel.melnyk10@gmail.com>
2024-12-11 18:18:24 +0100

    feat: implemented JWT support and Swagger UI configuration - Added JWT-based authentication support - Created `AuthenticationService`, `JwtAuthenticationFilter`, and `JwtUtil` classes - Updated `AuthenticationController` with `/api/auth/login` endpoint - Added Swagger UI with JWT authentication via `OpenApiConfig` class

---
+++ src/main/java/mate/academy/bookstore/dto/user/UserLoginRequestDto.java
@@ -0,0 +1,14 @@
+package mate.academy.bookstore.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.Length;
+
+public record UserLoginRequestDto(
+        @NotBlank
+        @Length(min = 8, max = 35)
+        @Email
+        String email,
+        String password
+) {
+}
