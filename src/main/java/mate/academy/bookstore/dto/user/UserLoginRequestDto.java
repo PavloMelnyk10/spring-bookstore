15ee69545cf0bc430e339fde636296a47ae0250a
Pavlo Melnyk <pavel.melnyk10@gmail.com>
2024-12-12 12:24:34 +0100

    refactor: removed password length validation during login

--- src/main/java/mate/academy/bookstore/dto/user/UserLoginRequestDto.java
+++ src/main/java/mate/academy/bookstore/dto/user/UserLoginRequestDto.java
@@ -2,11 +2,9 @@ package mate.academy.bookstore.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import org.hibernate.validator.constraints.Length;
 
 public record UserLoginRequestDto(
         @NotBlank
-        @Length(min = 8, max = 35)
         @Email
         String email,
         @NotBlank
