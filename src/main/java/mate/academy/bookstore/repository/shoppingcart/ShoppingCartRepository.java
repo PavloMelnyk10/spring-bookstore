d575311f43315de4e56ff36c9c752056e467a4d7
Pavlo Melnyk <112509930+PavloMelnyk10@users.noreply.github.com>
2024-12-16 11:43:33 +0000

    Update src/main/java/mate/academy/bookstore/repository/shoppingcart/ShoppingCartRepository.java
    Co-authored-by: Safarov Renat <98067127+JJJazl@users.noreply.github.com>

--- src/main/java/mate/academy/bookstore/repository/shoppingcart/ShoppingCartRepository.java
+++ src/main/java/mate/academy/bookstore/repository/shoppingcart/ShoppingCartRepository.java
@@ -8,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
 
     @EntityGraph(attributePaths = {"cartItems", "cartItems.book"})
-    Optional<ShoppingCart> findByUserId(Long userId);
+    Optional<ShoppingCart> findByUserIdFetchCartItemsWithBooks(Long userId);
 }
